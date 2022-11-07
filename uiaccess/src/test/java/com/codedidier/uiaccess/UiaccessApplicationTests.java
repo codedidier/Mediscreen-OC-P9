63f5ab2a16e5b030b99fe207112f3dcb6933a2ff
Didier Cotrel <didier.cotrel@gmail.com>
2022-11-07 14:21:59 +0100

    Implementation du micro-service uiaccess : pour la gestion du front ajout de Swagger et Feign

---
+++ uiaccess/src/test/java/com/codedidier/uiaccess/UiaccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codedidier.uiaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiaccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
